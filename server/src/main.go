695b061159f17e9428dbd7d5c389aa5bdd891881
AJ Clemente <areljann.c@gmail.com>
2020-05-01 18:19:40 -0400

    DEVELOP: Load environment variables

--- server/src/main.go
+++ server/src/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"server/api"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables.
+	err := godotenv.Load("../.env")
+	if err != nil {
+		fmt.Println("Error loading environment variables.")
+	}
+
 	// Initialize router.
 	router := api.GetRouter()
 
